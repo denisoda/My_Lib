0f3643550542bc1f94c8eee92e4a5fdc1d5dd270
Ilya Dzeraziak <ilya.by@bk.ru>
2017-11-15 21:13:08 +0300

    Added .NET brand lib

---
+++ C#/Brand_lib/My_Info.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace My_Info
+{
+     public static class Brand
+    {
+        public static void show_info()
+        {
+            Console.WriteLine("The creater of the project is " + Creater);
+        }
+        public const string Creater = "ILYA DZERAZIAK";
+
+    public static void show_logo()
+    {
+            string title = @" 
+
+                        _____ _               _____                        _       _     
+                       |_   _| |             |  __ \                      (_)     | |    
+                         | | | |_   _  __ _  | |  | |_______ _ __ __ _ _____  __ _| | __ 
+                         | | | | | | |/ _` | | |  | |_  / _ \ '__/ _` |_  / |/ _` | |/ / 
+                        _| |_| | |_| | (_| | | |__| |/ /  __/ | | (_| |/ /| | (_| |   <  
+                       |_____|_|\__, |\__,_| |_____//___\___|_|  \__,_/___|_|\__,_|_|\_\ 
+                                 __/ |____           _           _                       
+                                |___/  __ \         (_)         | |                      
+                                    | |__) | __ ___  _  ___  ___| |_                     
+                                    |  ___/ '__/ _ \| |/ _ \/ __| __|                    
+                                    | |   | | | (_) | |  __/ (__| |_                     
+                                    |_|   |_|  \___/| |\___|\___|\__|                    
+                                                   _/ |                                  
+                                                  |__/                                 
+                                                                                                                                                   ";
+
+            Console.WriteLine(title);
+        }
+    }
+}
